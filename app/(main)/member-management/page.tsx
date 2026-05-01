a0e4e5354c6d038dcfa829dc4c73e9061ead7619
payalgondane33 <payalgondane33@gmail.com>
2026-05-02 00:33:04 +0530

    feat: update styling code

--- app/(main)/member-management/page.tsx
+++ app/(main)/member-management/page.tsx
@@ -53,19 +53,8 @@ const MemberManagement = () => {
     <PageBreadcrumb items={[{ label: "Member Management" }]} />
     <div className="flex items-center justify-between mb-5 rounded-md p-4 border border-gray-200 bg-white dark:border-gray-800 dark:bg-white/[0.03]">
       <h3 className="text-lg font-semibold text-gray-800 dark:text-white/90">
-        Total Member {MOCK_MEMBERS.length}
-      </h3> 
-      <div className="mt-4 flex gap-2">
-        <Button variant='outline' size='sm'>All Member</Button>
-        <Button variant='outline' size='sm'>Active</Button>
-        <Button variant='outline' size='sm'>Expired</Button>
-        <Button variant='outline' size='sm'>Trial</Button>
-      </div>      
-      <input
-          type="text"
-          placeholder="Search..."          
-          className="min-w-[141px] rounded-md border border-gray-200 px-3 py-2 text-sm text-gray-600 placeholder:text-gray-400 dark:bg-white/[0.03] dark:border-white/[0.05] dark:text-white/90 ring-offset-0 focus:outline-none focus:ring-0 focus:ring-offset-0"
-        />
+        Total Member <span className="text-brand-500 font-bold text-2xl ml-2 border border-brand-500 rounded-md px-2 py-1">{MOCK_MEMBERS.length}</span>
+      </h3>        
     </div>
     <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {MOCK_MEMBERS.length > 0 && (MOCK_MEMBERS.map((member) => (
